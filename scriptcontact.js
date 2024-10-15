a28b9d2ef5982e5cbcdd8724493c2784e6ebfe27
CarlJosephDumalay <51311886+CarlJosephDumalay@users.noreply.github.com>
2024-10-15 13:09:06 +0800

    Removal of Form and Updating of designs

--- scriptcontact.js
+++ scriptcontact.js
@@ -1,25 +1,3 @@
-//JAVASCRIPT FOR CONTACT SESSION USING EMAILJS
-//JAVASCRIPT FOR CONTACT SESSION USING EMAILJS
-//JAVASCRIPT FOR CONTACT SESSION USING EMAILJS
-
-
-    // Initialize EmailJS with your User ID
-    (function() {
-        emailjs.init("MP0b1l5I64JvGz4dm"); // Replace with your EmailJS User ID
-    })();
-
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        // Send the form using EmailJS
-        emailjs.sendForm('service_h4g1pf2', 'template_ftjuuzx', this)
-            .then(function(response) {
-                document.getElementById('response-message').textContent = 'Thank you! Your message has been sent.';
-                document.getElementById('contact-form').reset(); // This clears the form
-            }, function(error) {
-                document.getElementById('response-message').textContent = 'Failed to send message. Please try again later.';
-            });
-    });
 
 
 //JAVASCRIPT FOR NAV SCROLL
